3aeebae7debb3b093bc0da6eaa006e529d7161f0
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-20 11:58:21 -0300

    Os métodos de validação

--- 02-calculator/src/main/java/br/com/francisco/MathController.java
+++ 02-calculator/src/main/java/br/com/francisco/MathController.java
@@ -15,15 +15,19 @@ public class MathController {
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
 			throw new Exception();
 		}
-		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
-		return sum;
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
 
-	private Double convertToDouble(String number) {
-		return 1D;
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
-	private boolean isNumeric(String number) {
-		return false;
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
 	}
 }
